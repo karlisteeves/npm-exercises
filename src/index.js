0e74f2bd5f5c06e4e349d888a56efcc56a13515f
Karli Steeves <steeves.karli@gmail.com>
2019-11-14 15:46:22 -0600

    working on the modules exercise

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const $ = require('jquery');
+import $ from 'jquery';
+
+const item = ('./practice.js');
+
 
 $(document).ready(function() {
     alert("Hello world!");
 });
 
 $(document).ready(function() {
-    $(body).css("background", "lavender");
+    $('body').css({"background-color" : "lavender"});
 })
 
 $("h3").html("If I Jump Out Of The 6th Floor, Will I Die or Just Get Hurt Real Bad?");
@@ -13,5 +16,7 @@ $("h3").html("If I Jump Out Of The 6th Floor, Will I Die or Just Get Hurt Real B
 function sayHello(){
     return "Hello!"
 }
+
+console.log(item.color);
 
 console.log(sayHello());
---
+++ src/practice.js
@@ -0,0 +1,17 @@
+export const item = {
+
+    name: "eggs",
+    weight: "baby chicken",
+    type: "silkie",
+    color: "blue"
+};
+
+
+export const thing = {
+    name: "milk",
+    weight: "more than eggs",
+    type: "chocolate",
+    color: "brown"
+};
+
+export default  = {item, thing};
